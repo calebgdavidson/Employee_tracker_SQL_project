384d5a0b23ccb30932f2201773dabee8c51586f6
Caleb Davidson <caleb.davidson@michaelkors.com>
2023-03-09 21:29:25 -0600

    adding seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (name)
+VALUES 
+('Marketing'),
+('IT'),
+('Management'),
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Marketing Associate', 12000, 3)
+('Web Developer', 22500, 2)
+('Manager', 30000, 3)
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES 
+('Mike', 'Wazowski', 123)
+('Jimmy', 'John', 456)
+('Boo', 'Ahhh', 789)
